9aa6fe37824213772d357a409109d91e6f8406e7
shoaibrafiq <u1464884@unimail.hud.ac.uk>
2017-01-17 12:13:54 +0000

    modified products list

--- resources/views/front-end/home.blade.php
+++ resources/views/front-end/home.blade.php
@@ -44,161 +44,86 @@
           </a>
 
       </header>
+
+
+    <!-- Page Content -->
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+              <h2>Check Out The Latest Stock</h2>
+                <div class="col-xs-12 col-sm-6 col-md-3">
+                    <div class="thumbnail" >
+                        <img src="http://placehold.it/700x350&text=1" class="img-responsive">
+                        <div class="caption">
+                            <div class="row">
+                                <div class="col-md-4 col-sm-4 col-xs-4 price">
+                                    <h3 style="margin:5px auto;"><label>$1</label></h3>
+                                </div>
+                                <div class="col-md-8 col-sm-8 col-xs-8">
+                                    <a href="{{route('jacket')}}" class="btn btn-success btn-product">View Jacket</a>
+                                </div>
+                            </div>
+
+                            <p> </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-3">
+                    <div class="thumbnail" >
+                        <img src="http://placehold.it/700x350&text=1" class="img-responsive">
+                        <div class="caption">
+                            <div class="row">
+                                <div class="col-md-4 col-sm-4 col-xs-4 price">
+                                    <h3 style="margin:5px auto;"><label>$2</label></h3>
+                                </div>
+                                <div class="col-md-8 col-sm-8 col-xs-8">
+                                      <a href="#" class="btn btn-success btn-product">View Jacket</a>
+                                </div>
+                            </div>
+
+                            <p> </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-3">
+                    <div class="thumbnail" >
+                        <img src="http://placehold.it/700x350&text=1" class="img-responsive">
+                        <div class="caption">
+                            <div class="row">
+                                <div class="col-md-4 col-sm-4 col-xs-4 price">
+                                    <h3 style="margin:5px auto;"><label>$3</label></h3>
+                                </div>
+                                <div class="col-md-8 col-sm-8 col-xs-8">
+                                    <a href="#" class="btn btn-success btn-product">View Jacket</a>
+                                </div>
+                            </div>
+
+                            <p> </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-3">
+                    <div class="thumbnail" >
+                        <img src="http://placehold.it/700x350&text=1" class="img-responsive">
+                        <div class="caption">
+                            <div class="row">
+                                <div class="col-md-4 col-sm-4 col-xs-4 price">
+                                    <h3 style="margin:5px auto;"><label>$4</label></h3>
+                                </div>
+                                <div class="col-md-8 col-sm-8 col-xs-8">
+                                      <a href="#" class="btn btn-success btn-product">View Jacket</a>
+                                </div>
+                            </div>
+
+                            <p> </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
   <hr>
-      <!-- Page Content -->
-      <div class="container">
-          <div class="row">
-              <div class="col-md-12">
-                  <div class="col-xs-12 col-sm-6 col-md-3">
-                      <div class="thumbnail" >
-                          <img src="http://placehold.it/700x350&text=1" class="img-responsive">
-                          <div class="caption">
-                              <div class="row">
-                                  <div class="col-md-4 col-sm-4 col-xs-4 price">
-                                      <h3 style="margin:5px auto;"><label>$1</label></h3>
-                                  </div>
-                                  <div class="col-md-8 col-sm-8 col-xs-8">
-                                      <a href="#" class="btn btn-success btn-product"><span class="glyphicon glyphicon-shopping-cart"></span> Add 2 Cart</a>
-                                  </div>
-                              </div>
-
-                              <p> </p>
-                          </div>
-                      </div>
-                  </div>
-                  <div class="col-xs-12 col-sm-6 col-md-3">
-                      <div class="thumbnail" >
-                          <img src="http://placehold.it/700x350&text=1" class="img-responsive">
-                          <div class="caption">
-                              <div class="row">
-                                  <div class="col-md-4 col-sm-4 col-xs-4 price">
-                                      <h3 style="margin:5px auto;"><label>$2</label></h3>
-                                  </div>
-                                  <div class="col-md-8 col-sm-8 col-xs-8">
-                                      <a href="#" class="btn btn-success btn-product"><span class="glyphicon glyphicon-shopping-cart"></span> Add 2 Cart</a>
-                                  </div>
-                              </div>
-
-                              <p> </p>
-                          </div>
-                      </div>
-                  </div>
-                  <div class="col-xs-12 col-sm-6 col-md-3">
-                      <div class="thumbnail" >
-                          <img src="http://placehold.it/700x350&text=1" class="img-responsive">
-                          <div class="caption">
-                              <div class="row">
-                                  <div class="col-md-4 col-sm-4 col-xs-4 price">
-                                      <h3 style="margin:5px auto;"><label>$3</label></h3>
-                                  </div>
-                                  <div class="col-md-8 col-sm-8 col-xs-8">
-                                      <a href="#" class="btn btn-success btn-product"><span class="glyphicon glyphicon-shopping-cart"></span> Add 2 Cart</a>
-                                  </div>
-                              </div>
-
-                              <p> </p>
-                          </div>
-                      </div>
-                  </div>
-                  <div class="col-xs-12 col-sm-6 col-md-3">
-                      <div class="thumbnail" >
-                          <img src="http://placehold.it/700x350&text=1" class="img-responsive">
-                          <div class="caption">
-                              <div class="row">
-                                  <div class="col-md-4 col-sm-4 col-xs-4 price">
-                                      <h3 style="margin:5px auto;"><label>$4</label></h3>
-                                  </div>
-                                  <div class="col-md-8 col-sm-8 col-xs-8">
-                                      <a href="#" class="btn btn-success btn-product"><span class="glyphicon glyphicon-shopping-cart"></span> Add 2 Cart</a>
-                                  </div>
-                              </div>
-
-                              <p> </p>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-
-          </div>
-      </div>
-
-
-              <hr>
-              <div class="container">
-          <div class="row">
-              <div class="col-md-12">
-                  <div class="col-xs-12 col-sm-6 col-md-3">
-                      <div class="thumbnail" >
-                          <img src="http://placehold.it/700x350&text=1" class="img-responsive">
-                          <div class="caption">
-                              <div class="row">
-                                  <div class="col-md-4 col-sm-4 col-xs-4 price">
-                                      <h3 style="margin:5px auto;"><label>$1</label></h3>
-                                  </div>
-                                  <div class="col-md-8 col-sm-8 col-xs-8">
-                                      <a href="#" class="btn btn-success btn-product"><span class="glyphicon glyphicon-shopping-cart"></span> Add 2 Cart</a>
-                                  </div>
-                              </div>
-
-                              <p> </p>
-                          </div>
-                      </div>
-                  </div>
-                  <div class="col-xs-12 col-sm-6 col-md-3">
-                      <div class="thumbnail" >
-                          <img src="http://placehold.it/700x350&text=1" class="img-responsive">
-                          <div class="caption">
-                              <div class="row">
-                                  <div class="col-md-4 col-sm-4 col-xs-4 price">
-                                      <h3 style="margin:5px auto;"><label>$2</label></h3>
-                                  </div>
-                                  <div class="col-md-8 col-sm-8 col-xs-8">
-                                      <a href="#" class="btn btn-success btn-product"><span class="glyphicon glyphicon-shopping-cart"></span> Add 2 Cart</a>
-                                  </div>
-                              </div>
-
-                              <p> </p>
-                          </div>
-                      </div>
-                  </div>
-                  <div class="col-xs-12 col-sm-6 col-md-3">
-                      <div class="thumbnail" >
-                          <img src="http://placehold.it/700x350&text=1" class="img-responsive">
-                          <div class="caption">
-                              <div class="row">
-                                  <div class="col-md-4 col-sm-4 col-xs-4 price">
-                                      <h3 style="margin:5px auto;"><label>$3</label></h3>
-                                  </div>
-                                  <div class="col-md-8 col-sm-8 col-xs-8">
-                                      <a href="#" class="btn btn-success btn-product"><span class="glyphicon glyphicon-shopping-cart"></span> Add 2 Cart</a>
-                                  </div>
-                              </div>
-
-                              <p> </p>
-                          </div>
-                      </div>
-                  </div>
-                  <div class="col-xs-12 col-sm-6 col-md-3">
-                      <div class="thumbnail" >
-                          <img src="http://placehold.it/700x350&text=1" class="img-responsive">
-                          <div class="caption">
-                              <div class="row">
-                                  <div class="col-md-4 col-sm-4 col-xs-4 price">
-                                      <h3 style="margin:5px auto;"><label>$4</label></h3>
-                                  </div>
-                                  <div class="col-md-8 col-sm-8 col-xs-8">
-                                      <a href="#" class="btn btn-success btn-product"><span class="glyphicon glyphicon-shopping-cart"></span> Add 2 Cart</a>
-                                  </div>
-                              </div>
-
-                              <p> </p>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-
-          </div>
-      </div>
-      <hr>
 
 @endsection
