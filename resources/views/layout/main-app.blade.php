fd15e367070bb9e666b7ff0a610e49cb552d3895
shoaibrafiq <u1464884@unimail.hud.ac.uk>
2017-01-17 10:48:17 +0000

    changes to layout

--- resources/views/layout/main-app.blade.php
+++ resources/views/layout/main-app.blade.php
@@ -7,20 +7,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>ICB Jackets</title>
+    <title>@yield('title','ICB Jackets')</title>
 
     <!-- Bootstrap Core CSS -->
-    <link href="{{asset(dist/css/bootstrap.min.css)}}" rel="stylesheet">
+    <link href="{{asset('dist/css/bootstrap.min.css')}}" rel="stylesheet">
 
     <!-- Custom CSS -->
-    <link href="{{asset(dist/css/full-slider.css)}}" rel="stylesheet">
+    <link href="{{asset('dist/css/full-slider.css')}}" rel="stylesheet">
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
 
 </head>
 
@@ -67,10 +61,10 @@
     <!-- /.container -->
 
     <!-- jQuery -->
-    <script src="{{asset(dist/js/jquery.js)}}"></script>
+    <script src="{{asset('dist/js/jquery.js')}}"></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="{{asset(dist/js/bootstrap.min.js}}"></script>
+    <script src="{{asset('dist/js/bootstrap.min.js')}}"></script>
 
     <!-- Script to Activate the Carousel -->
     <script>
