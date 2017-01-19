d54e6cb1a750736ace1b649559e0caa9c5015787
shoaibrafiq <u1464884@unimail.hud.ac.uk>
2017-01-19 12:28:16 +0000

    Created a new dashboard for the admin and deleted old one

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,5 +1,19 @@
 @extends('admin.layout.admin')
 @section('content')
 
-  
+  <div style="margin-left:50px;"class="container">
+      <div class="row">
+          <div class="col-md-8 col-md-offset-2">
+              <div class="panel panel-default">
+                  <div class="panel-heading">Dashboard</div>
+
+                  <div class="panel-body">
+                      Welcome to the Admin page!
+<p>Here you can view, add, edit products and categories which will be displayed in the main website.</p>
+
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
 @endsection
