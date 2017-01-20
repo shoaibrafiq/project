e1e2b3a98549fb784c223225c9ff3c9fa9332d53
shoaibrafiq <u1464884@unimail.hud.ac.uk>
2017-01-20 13:36:46 +0000

    Changed product index

--- resources/views/admin/product/index.blade.php
+++ resources/views/admin/product/index.blade.php
@@ -9,26 +9,23 @@
   box-shadow: 1px 2px 4px rgba(0, 0, 0, .5); display: block;"class="col-md-12 col-md-offset-2 row">
 <h3>Products</h3>
 
-<ul>
+
   @forelse ($products as $product)
-      <li>
-  <h4>Name: {{$product->name}}</h4>
+
+  <h4>Product ID: {{$product->id}} Product Name: {{$product->name}}</h4>
   <form action="{{route('product.destroy',$product->id)}}"  method="POST">
           {{csrf_field()}}
            {{method_field('DELETE')}}
           <input class="btn btn-sm btn-danger" type="submit" value="Delete">
         </form>
- </form>
-
  <a class="btn btn-sm btn-success" href="{{route('product.edit', $product->id)}}">Edit</a>
 
-  </li>
 
 @empty
 
   <h3>No Products to show</h3>
 @endforelse
-</ul>
+
 
   </div>
 
